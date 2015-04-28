becdff9561ae035f89c706a48c280e40be2b2956
Nguyen Huu Hanh Lam <nguyenhuuhanhlam@gmail.com>
2015-04-28 16:29:43 +0700

    ...

--- public/index.php
+++ public/index.php
@@ -1,13 +1,33 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <link rel="stylesheet" href="css/metro-bootstrap.css">
-        <script src="js/jquery-2.1.3.min.js"></script>
-        <script src="js/jquery-ui.min.js"></script>
-        <script src="js/jquery.mousewheel.min.js"></script>
-        <script src="js/metro/metro.min.js"></script>
-    </head>
-    <body class="metro">
-        ... markup page TEST 1
-    </body>
+<head>
+<link rel="stylesheet" href="css/metro-bootstrap.css">
+<link rel="stylesheet" href="css/default.css">
+<script src="js/jquery-2.1.3.min.js"></script>
+<script src="js/jquery-ui.min.js"></script>
+<script src="js/jquery.mousewheel.min.js"></script>
+<script src="js/metro/metro.min.js"></script>
+</head>
+<body class="metro">
+	<div>
+		BAN VE<br> 1. chon ngay<br> 2. danh sach phim dang chieu (chi hien
+		ngay hien hanh + tai thoi diem + va nhung ngay sau do)<br>
+
+		<div style="background-color: #cccccc">
+			<div>Avenger 2 | xuat chieu | xuat chieu</div>
+			<div>Mad Max | xuat chieu</div>
+		</div>
+		3. ghe <br>
+
+		<div role="room">
+			<div role="seat" id="H-1" value="H1">H1</div>
+			<div role="seat" type="VIP" id="H-2">H2</div>
+			<div role="seat" type="VIP" id="H-3">H3</div>
+			<div role="seat" id="H-4">H4</div>
+			<div role="seat" id="H-5">H5</div>
+			<div role="seat" id="Z" type="VIP">Z</div>
+		</div>
+
+	</div>
+</body>
 </html>
